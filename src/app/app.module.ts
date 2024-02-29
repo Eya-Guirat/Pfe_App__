2c84a6717c0330d018f754ea8d4d979a8e557245
Eya-Guirat <guirateya8@gmail.com>
2024-02-29 15:31:39 +0100

    First Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MainHeaderComponent } from './main-header/main-header.component';
+import { MainSidebarComponent } from './main-sidebar/main-sidebar.component';
+import { ContentWrapperComponent } from './content-wrapper/content-wrapper.component';
+import { ControlSidebarComponent } from './control-sidebar/control-sidebar.component';
+import { MainFooterComponent } from './main-footer/main-footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainHeaderComponent,
+    MainSidebarComponent,
+    ContentWrapperComponent,
+    ControlSidebarComponent,
+    MainFooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
